236996d69d40d36682913a5e3c5027483fb128d0
Jeffrey Zhou <jeffreyzhou@Jeffreys-MacBook-Pro.local>
2019-06-13 19:30:18 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PokemonMap/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import PokemonMap from './components/PokemonMap.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    PokemonMap
   }
 }
 </script>
